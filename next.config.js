32233d68326a9bbf490490877e1ac169adfc0af0
brandlew <hello@brandlew.co>
2024-05-27 19:10:16 -0700

    add back device size

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ module.exports = {
   output: 'export',
   images: {
     loader: "custom",
-    imageSizes: [480,768,1280,2048]
+    imageSizes: [480,768,1280,2048],
+    deviceSizes: [480,768,1280,2048],
   },
   transpilePackages: ["next-image-export-optimizer"],
   env: {
