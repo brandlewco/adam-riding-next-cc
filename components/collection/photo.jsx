01705ae4c1d76bc2cd1bfb49d30026ffcd89515b
brandlew <hello@brandlew.co>
2024-05-27 19:02:55 -0700

    update image size

--- components/collection/photo.jsx
+++ components/collection/photo.jsx
@@ -1,18 +1,44 @@
 import MarkdownIt from "markdown-it";
 import ExportedImage from "next-image-export-optimizer";
+import { useState, useEffect } from "react";
+
 
 const md = new MarkdownIt({ html: true });
 
 export default function CollectionPhoto({ block, dataBinding }) {
+
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = block.image_path;
+    img.onload = () => {
+      const aspectRatio = img.width / img.height;
+      setDimensions({
+        width: 2048, // Example width
+        height: 2048 / aspectRatio, // Calculate height based on the aspect ratio
+      });
+    };
+  }, [block.image_path]);
+
+  if (dimensions.width === 0 || dimensions.height === 0) {
+    return <div>Loading...</div>;
+  }
+
+
+
   return (
+    
     <section  data-cms-bind={dataBinding} className="photo flex justify-end items-start w-auto relative" style={{ height: '90vh' }}>
-        <ExportedImage
-          src={block.image_path}
-          alt={block.alt_text || "Slide Image"}
-          fill
-          style={{ objectFit: "cover" }}
-          priority
-        />
+      <ExportedImage
+        src={block.image_path}
+        alt={block.alt_text || "Slide Image"}
+        priority
+        width={dimensions.width}
+        height={dimensions.height}
+        layout="responsive"
+        objectFit="cover"
+      />
     </section>
   );
 }
